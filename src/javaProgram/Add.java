3ff561f2276ab1c511d00e7d26f85fef63bb1cfe
sarita <sarita.chetak@gmail.com>
2019-08-11 07:53:43 +0100

    sarita

---
+++ src/javaProgram/Add.java
@@ -0,0 +1,11 @@
+package javaProgram;
+
+public class Add {
+    public static void main(String[] args) {
+        int x,y,result;
+        x=30;
+        y=10;
+        result=x+y;
+        System.out.println("result= "+result);
+    }
+}
